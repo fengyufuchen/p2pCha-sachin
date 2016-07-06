a06b94b2602ad92c9c4aa6757a2ab6611cd25373
Sachin <402948352@qq.com>
2016-07-06 17:18:37 +0800

    增加会话功能

---
+++ src/com/chat/fun/PrivateConversationContext.java
@@ -0,0 +1,40 @@
+package com.chat.fun;
+
+import com.bean.PeerNode;
+import com.chat.uiview.PrivateConversationFrame;
+
+public class PrivateConversationContext {
+	private PrivateConversationFrame conFrame;
+	PeerNode conTo;
+	private String conToPeerName;
+
+	public PrivateConversationContext(String toName) {
+		this.conToPeerName = toName;
+		conFrame = new PrivateConversationFrame(toName);
+		conFrame.setVisible(true);
+
+	}
+
+	public PrivateConversationFrame getConFrame() {
+		return conFrame;
+	}
+
+	public void setConFrame(PrivateConversationFrame conFrame) {
+		this.conFrame = conFrame;
+	}
+
+	public PeerNode getConTo() {
+		return conTo;
+	}
+
+	public void setConTo(PeerNode conTo) {
+		this.conTo = conTo;
+	}
+
+	public void addReceiveMsg(String msgContent) {
+
+		conFrame.showReceiveMsg(msgContent);
+
+	}
+
+}
---
+++ src/com/chat/msgpacket/PrivateMessage.java
@@ -0,0 +1,22 @@
+package com.chat.msgpacket;
+
+import java.io.Serializable;
+
+import com.bean.PeerNode;
+
+public class PrivateMessage extends Message implements Serializable {
+	private PeerNode to;
+
+	public PrivateMessage(PeerNode from, PeerNode to, String imsg) {
+		super(imsg, from);
+		// TODO Auto-generated constructor stub
+		this.to = to;
+	}
+
+	@Override
+	public char getTag() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
